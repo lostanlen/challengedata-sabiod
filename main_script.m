798bc26772dfc1793372a65486a458271a1b1ee2
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-05 15:20:19 +0100

    get UUIDs of training set

--- main_script.m
+++ main_script.m
@@ -1,16 +1,15 @@
-train_prefix = 'BIRD50_TRAIN_LifeClefSABIOD2015_ID';
+extension = '.wav';
 
+%% Get UUIDS of training set
+train_prefix = 'BIRD50_TRAIN_LifeClefSABIOD2015_ID';
 train_files = dir('TRAIN');
 train_bytes = [train_files.bytes];
 train_files = train_files(train_bytes~=0);
 train_filenames = {train_files.name};
-
 nTrain_files = length(train_files);
 train_uuids = cell(1,nTrain_files);
-
 for train_file_index = 1:nTrain_files
-    train_uuids{train_file_index} = ...
-        train_filenames{train_file_index}((length(train_prefix)+1):(length(train_prefix)+8));
+    filename = train_filenames{train_file_index};
+    range = (length(train_prefix)+1):(length(filename)-length(extension));
+    train_uuids{train_file_index} = train_filenames{train_file_index}(range);
 end
-
-uuid_decimals = hex2dec(train_uuids)
