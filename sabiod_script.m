64d293e6483f58bb8286ebd251ea83e184009d6a
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-06 09:50:51 +0100

    put CSV parsing and WAV renaming in a single loop

--- sabiod_script.m
+++ sabiod_script.m
@@ -1,30 +1,35 @@
-%% Rename training set WAV files
+%% TRAINING SET
+% List WAV files
 train_files = dir('TRAIN');
+% Remove zero-byte files
 train_bytes = [train_files.bytes];
 train_files = train_files(train_bytes~=0);
+% Get file names
 train_filenames = {train_files.name};
-nTrain_files = length(train_files);
-for train_file_index = 1:nTrain_files
-    filename = train_filenames{train_file_index};
-    filepath_in = ['TRAIN/', filename];
-    filepath_out = ['TRAIN/', 'ID', num2str(train_file_index, '%0.4d'), '.wav'];
-    movefile(filepath_in, filepath_out)
-end
-
-%% Rename files in training set CSV
+nTrain_WAV_files = length(train_files);
+% Load CSV lines
 train_fid = fopen('SABIOD_LIFECLEF_UTLN_BIRD50_output_training.csv');
 train_csv_cells = textscan(train_fid, '%s%s', 'delimiter',',');
 fclose(train_fid);
-train_csv_cells = [train_csv_cells{:}];
+% Check that there are as many CSV lines as WAV files
+nCSV_lines = size(train_csv_cells, 1);
+assert(nTrain_WAV_files == nCSV_lines);
+nTrain_files = nTrain_WAV_files;
+% Initialize new CSV file
 train_csv_lines = cell(1, nTrain_files);
 line_break = char(10);
 for train_file_index = 1:nTrain_files
-    train_csv_lines{train_file_index} = ...
-        ['ID', num2str(train_file_index, '%0.4d'), ...
-         ',', ...
-         train_csv_cells{train_file_index,2}, ...
-         line_break];
+    % Get UUID
+    % Rename UUID WAV file to its ID
+    filepath_in = ['TRAIN/', filename];
+    filepath_out = ['TRAIN/', 'ID', num2str(train_file_index, '%0.4d'), '.wav'];
+    movefile(filepath_in, filepath_out);
+    % Generate CSV line
+    id_str = num2str(train_file_index, '%0.4d');
+    class_str = train_csv_cells{train_file_index, 2};
+    train_csv_line = ['ID', id_str, ',', class_str, line_break];
 end
+% Write to CSV
 train_fid = fopen('train.csv','w');
 fprintf(train_fid, '%s', train_csv_lines{:});
 fclose(train_fid);
