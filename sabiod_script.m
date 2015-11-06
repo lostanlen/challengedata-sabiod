6fb401b9f073a3181eab547ee212a6a740579cf8
Vincent Lostanlen <vincent.lostanlen@ens.fr>
2015-11-06 10:15:03 +0100

    finish training set part

--- sabiod_script.m
+++ sabiod_script.m
@@ -1,6 +1,9 @@
 %% TRAINING SET
 % List WAV files
+train_prefix = 'BIRD50_TRAIN_LifeClefSABIOD2015_';
+extension = '.wav';
 train_files = dir('TRAIN');
+mkdir('TRAIN_out')
 % Remove zero-byte files
 train_bytes = [train_files.bytes];
 train_files = train_files(train_bytes~=0);
@@ -20,47 +23,18 @@ train_csv_lines = cell(1, nTrain_files);
 line_break = char(10);
 for train_file_index = 1:nTrain_files
     % Get UUID
-    % Rename UUID WAV file to its ID
-    filepath_in = ['TRAIN/', filename];
-    filepath_out = ['TRAIN/', 'ID', num2str(train_file_index, '%0.4d'), '.wav'];
-    movefile(filepath_in, filepath_out);
+    train_uuid = train_csv_cells{train_file_index, 1};
+    % Generate ID
+    train_id = num2str(train_file_index, '%0.4d');
+    % Rename WAV file
+    train_filepath_in = ['TRAIN/', train_prefix, train_uuid, extension];
+    train_filepath_out = ['TRAIN_out/', 'ID', train_id, extension]
+    movefile(train_filepath_in, train_filepath_out);
     % Generate CSV line
-    id_str = num2str(train_file_index, '%0.4d');
-    class_str = train_csv_cells{train_file_index, 2};
-    train_csv_line = ['ID', id_str, ',', class_str, line_break];
+    train_class = train_csv_cells{train_file_index, 2};
+    train_csv_line = ['ID', train_id, ',', train_class, line_break];
 end
 % Write to CSV
 train_fid = fopen('train.csv','w');
 fprintf(train_fid, '%s', train_csv_lines{:});
 fclose(train_fid);
-
-%% Rename test set WAV files
-test_files = dir('TEST');
-test_bytes = [test_files.bytes];
-test_files = test_files(test_bytes~=0);
-test_filenames = {test_files.name};
-nTest_files = length(test_files);
-for test_file_index = 1:nTest_files
-    filename = test_filenames{test_file_index};
-    filepath_in = ['TEST/', filename];
-    filepath_out = ['TEST/', 'ID', num2str(1000+test_file_index, '%0.4d'), '.wav'];
-    movefile(filepath_in, filepath_out);
-end
-
-%% Rename files in testing set CSV
-test_fid = fopen('SABIOD_LIFECLEF_UTLN_BIRD50v2_output_testing.csv');
-test_csv_cells = textscan(test_fid, '%s%s', 'delimiter',',');
-fclose(test_fid);
-test_csv_cells = [test_csv_cells{:}];
-test_csv_lines = cell(1, nTest_files);
-line_break = char(10);
-for test_file_index = 1:nTest_files
-    test_csv_lines{test_file_index} = ...
-        ['ID', num2str(1000 + test_file_index, '%0.4d'), ...
-         ',', ...
-         test_csv_cells{test_file_index,2}, ...
-         line_break];
-end
-test_fid = fopen('test.csv','w');
-fprintf(test_fid, '%s', test_csv_lines{:});
-fclose(test_fid);
